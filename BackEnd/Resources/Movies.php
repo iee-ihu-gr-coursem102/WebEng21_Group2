b181dfbc36751a42a0a62917375a3f064cf50edb
KardamanidisChristos <33559854+KardamanidisChristos@users.noreply.github.com>
2021-11-29 14:42:03 +0200

    Added Template of Movies Resource

--- BackEnd/Resources/Movies.php
+++ BackEnd/Resources/Movies.php
@@ -1,4 +1,13 @@
 <?php
 
-print "Movies Resource";
+$data[] = array(
+    "MOVIEID" => 1, 
+    "DESCRIPTION" => "Captain Jack Sparrow ", 
+    "TITLE" => "Pirates of Caribean: The Curse of the Black pearl", 
+    "LANGUAGE" => "Greek", 
+    "POSTER_IMAGE" => "Images/piratesOfCaribean.jpg", 
+    "GENREID" => 1
+);
+echo json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+
 ?>
