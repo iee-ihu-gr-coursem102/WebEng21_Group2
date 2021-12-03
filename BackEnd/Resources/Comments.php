7a2f72ae25a5e16be246460c5f70e34f4e1d532c
KardamanidisChristos <33559854+KardamanidisChristos@users.noreply.github.com>
2021-12-03 03:11:10 +0200

    Comments and Sessions Resource Managment with demo data

--- BackEnd/Resources/Comments.php
+++ BackEnd/Resources/Comments.php
@@ -1,4 +1,21 @@
 <?php
 
-print "Comments Resource";
+if ($httpMethod == "POST") {
+	if (isset($json["userid"]) && isset($json["movied"]) && isset($json["commentText"])) {
+		//Insert in DB
+		
+		$data = array("commentid" => 1); // Demo data for now
+		echo json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+		header('HTTP/1.1 201 Created');
+	} else {
+		header('HTTP/1.1 400 Bad Request');
+	}
+} else if ($httpMethod == "GET") {
+	header('HTTP/1.1 501 Not Implemented');
+} else if ($httpMethod == "DELETE") {
+	header('HTTP/1.1 501 Not Implemented');
+}
+else {
+	header('HTTP/1.1 405 Method Not Allowed');
+}
 ?>
