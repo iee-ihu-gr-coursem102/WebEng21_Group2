188293d093ea2723a75a5f5e36e1dea8252df266
KardamanidisChristos <33559854+KardamanidisChristos@users.noreply.github.com>
2021-12-12 15:57:38 +0200

    Implementation of Genres Resource
    Web API returns all categories.

--- BackEnd/Resources/Genres.php
+++ BackEnd/Resources/Genres.php
@@ -1,7 +1,15 @@
 <?php
 if ($httpMethod == "GET") {
-	$data = array("Drama", "Action", "Romance");
-	echo json_encode($data);
+
+	$result = mysqli_query($mysqli, "SELECT DISTINCT SUBSTRING(GENRES,1, POSITION(',' IN GENRES)-1 ) AS CATEGORY FROM movies ORDER BY GENRES;");
+	if($result) {
+		$categories = array();
+		while ($row = $result->fetch_assoc()) {
+			if ($row["CATEGORY"] != "")
+				array_push($categories, $row["CATEGORY"]);
+		}
+		echo json_encode($categories, JSON_PRETTY_PRINT);
+	}
 	header('HTTP/1.1 200 ΟΚ');
 }
 
