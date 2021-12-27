fa2015ca770e81862b6c7fa90a6c5566f1f4a61a
athina-m <44371673+athina-m@users.noreply.github.com>
2021-12-27 16:24:25 +0200

    insert comment in db

--- BackEnd/Resources/Comments.php
+++ BackEnd/Resources/Comments.php
@@ -1,18 +1,37 @@
 <?php
 
-if ($httpMethod == "POST") {
-	if (isset($json["userid"]) && isset($json["movied"]) && isset($json["commentText"])) {
+if ($httpMethod == "POST") 
+{
+	if (isset($json["userid"]) && isset($json["movieid"]) && isset($json["commentText"])) 
+	{
 		//Insert in DB
-		
-		$data = array("commentid" => 1); // Demo data for now
-		echo json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-		header('HTTP/1.1 201 Created');
-	} else {
+		$sql = "INSERT INTO reviews (USER_ID, IMDB_ID , REVIEW_TEXT, DATE_OF_REVIEW) VALUES(?, ?, ?, ?)";
+		$stmt = $mysqli->prepare($sql);
+		$stmt->bind_param("sss", $json["userid"], $json["movieid"], $json["commentText"], date("Y-m-d H:i:s") /* get instant Datetime */);
+
+		if($stmt->execute()) 
+		{
+			$stmt->close();
+			/*$data = array("commentid" => 1); // Demo data for now
+		echo json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);*/
+			header('HTTP/1.1 201 Created');
+		} 
+		else 
+		{
+			print $stmt->error;
+			header('HTTP/1.1 500 Internal Server Error');
+		}
+	} 
+	else 
+	{
 		header('HTTP/1.1 400 Bad Request');
 	}
-} else if ($httpMethod == "GET") {
+} 
+else if ($httpMethod == "GET") 
+{
 	header('HTTP/1.1 501 Not Implemented');
-} else if ($httpMethod == "DELETE") {
+} 
+else if ($httpMethod == "DELETE") {
 	header('HTTP/1.1 501 Not Implemented');
 }
 else {
