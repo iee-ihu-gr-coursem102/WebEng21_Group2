e38a8607119dfc429477197ac109881f06fda1fe
athina-m <44371673+athina-m@users.noreply.github.com>
2021-12-27 16:32:21 +0200

    get reviews from db

--- BackEnd/Resources/Comments.php
+++ BackEnd/Resources/Comments.php
@@ -2,12 +2,12 @@
 
 if ($httpMethod == "POST") 
 {
-	if (isset($json["userid"]) && isset($json["movieid"]) && isset($json["commentText"])) 
+	if (isset($json["userId"]) && isset($json["movieId"]) && isset($json["commentText"])) 
 	{
 		//Insert in DB
 		$sql = "INSERT INTO reviews (USER_ID, IMDB_ID , REVIEW_TEXT, DATE_OF_REVIEW) VALUES(?, ?, ?, ?)";
 		$stmt = $mysqli->prepare($sql);
-		$stmt->bind_param("sss", $json["userid"], $json["movieid"], $json["commentText"], date("Y-m-d H:i:s") /* get instant Datetime */);
+		$stmt->bind_param("sss", $json["userId"], $json["movieId"], $json["commentText"], date("Y-m-d H:i:s") /* get instant Datetime */);
 
 		if($stmt->execute()) 
 		{
@@ -29,9 +29,37 @@ if ($httpMethod == "POST")
 } 
 else if ($httpMethod == "GET") 
 {
-	header('HTTP/1.1 501 Not Implemented');
+	$sql_query = "SELECT * FROM reviews ";
+  
+	$result = mysqli_query($mysqli, $sql_query);
+
+	if($result) 
+    {
+		$comments = array();
+		while ($row = $result->fetch_assoc()) 
+        {
+			if ($row != null)
+			{ 
+                /*array_push($movies, $row);*/
+                $comments[] = array
+				(
+                    "movieId" => $row['IMDB_ID'], 
+                    "userId" => $row['USER_ID'], 
+                    "commentText" => $row['REVIEW_TEXT'], 
+					"commentDate" => $row['DATE_OF_REVIEW'], 
+				);
+            }
+		}
+		echo json_encode($comments, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+		header('HTTP/1.1 200 ΟΚ');
+	}
+	else
+	{
+		header('HTTP/1.1 501 Not Implemented');
+	}
 } 
-else if ($httpMethod == "DELETE") {
+else if ($httpMethod == "DELETE") 
+{
 	header('HTTP/1.1 501 Not Implemented');
 }
 else {
