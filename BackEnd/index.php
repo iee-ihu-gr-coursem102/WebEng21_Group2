2f0f97cd101f32f64dea6812218a53e9d07a7b36
KardamanidisChristos <33559854+KardamanidisChristos@users.noreply.github.com>
2022-01-03 23:35:19 +0200

    Accepted devs live servers as allowed origin

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -49,12 +49,17 @@ $request = array("dbversion" => $request[0], "resource" => $request[1]);
 
 
 if ($httpMethod == "OPTIONS") {
-	header('Access-Control-Allow-Origin:https://www.teithe.gr');
+	header('Access-Control-Allow-Origin:http://www.teithe.gr');
 	header('Access-Control-Allow-Methods: GET,DELETE,POST');
 	exit;
 }
 if ($request["dbversion"]) {
-	header('Access-Control-Allow-Origin: https://www.teithe.gr');
+    if($_SERVER["HTTP_ORIGIN"] == "http://127.0.0.1:5503") { 
+		header('Access-Control-Allow-Origin: http://127.0.0.1:5503'); } 
+    else { 
+         header('Access-Control-Allow-Origin: https://www.teithe.gr');
+    }
+	
     if ($request["resource"] == "Movies") {
         require_once "./Resources/" . $request['resource'] . ".php";
     }
