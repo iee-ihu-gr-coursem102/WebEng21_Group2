357380e348727068ec3cd63ff336360fe14230ff
stelsar123 <ssaroglou1996@gmail.com>
2022-01-13 02:08:03 +0200

    Comments/Reviews

--- FrontEnd/js/movie_page.js
+++ FrontEnd/js/movie_page.js
@@ -27,6 +27,56 @@ document.getElementById("overview").innerHTML = json.overview;
 
 
 document.getElementById("images").src = json.posterImage;
+const postedComments = document.getElementById("postedComments");
+
+const loadComment = async () => {
+    try {
+       
+            const id = json.id;
+            const res = await fetch("https://users.it.teithe.gr/~ait062021/index.php/v1/Comments?movieId="+id);
+            comments = await res.json();
+            displayComments(comments);
+        
+        
+        } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayComments = (comments) => {
+    const htmlString = comments
+        .map((commnets) => {
+            return `
+            
+            <div class="be-comment">
+                <div class="be-img-comment">	
+                 
+                    <img src="img/duck.png" alt="" class="be-ava-comment">
+                 
+                </div>
+                <div class="be-comment-content">
+                  
+                    <span class="be-comment-name">
+                      ${commnets.username}
+                      </span>
+                    <span class="be-comment-time">
+                      ${commnets.commentDate}
+                    </span>
+            
+                  <p class="commentText" style="color:white">
+                  ${commnets.commentText}
+                  </p>
+                </div>
+              </div>
+            
+            
+        `;
+        })
+        .join('');
+        postedComments.innerHTML = htmlString;
+};
+
+loadComment();
 
 //document.getElementById("sendButton").addEventListener("click", userInput);
 "use strict";
@@ -53,3 +103,22 @@ function userInput() {
  }
 
 
+ function userInput1() {
+    const id = json.id;
+    var userComment = document.getElementById('userComment').value;
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.open("POST", "https://users.it.teithe.gr/~ait062021/index.php/v1/Comments", false);
+    xhttp.onload = function() {
+    if (xhttp.status == 201){
+        alert("ok");
+        location.reload();
+    }else if (xhttp.readyState == 4 && xhttp.status == 400)
+        window.alert("Error");
+    
+    
+    }
+    xhttp.withCredentials = true;
+    xhttp.send('{"movieId" : "' + id + '", "commentText" : "' + userComment + '"}');
+
+ }
